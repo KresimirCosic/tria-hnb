7c4550de2f2333c311018a73824215d2325bc5ca
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2024-06-06 12:33:30 +0200

    refactor: cleanup and refactor
    formatting enabled via prettier and script in package.json, lint as well

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
+import CssBaseline from '@mui/material/CssBaseline';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './styles/index.scss';
 import App from './App.tsx';
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
