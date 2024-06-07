4c973445451ed58cb4e6d8280b5039ff50fc50b9
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2024-06-07 18:21:33 +0200

    feat: file organization change, home page structure

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,10 +1,19 @@
+import Fab from '@mui/material/Fab';
+import EuroIcon from '@mui/icons-material/Euro';
+import DollarIcon from '@mui/icons-material/AttachMoney';
+
 import DefaultLayout from '../../layouts/DefaultLayout/DefaultLayout';
 
 const HomePage: React.FC = () => {
   return (
     <DefaultLayout>
       <div className="page page-home">
-        <h1>HomePage</h1>
+        <Fab color="primary" aria-label="add">
+          <DollarIcon />
+        </Fab>
+        <Fab color="primary" aria-label="add">
+          <EuroIcon />
+        </Fab>
       </div>
     </DefaultLayout>
   );
